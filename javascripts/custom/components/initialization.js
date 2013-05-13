d050f82c9981dcfe55367a30f832c3184faed390
Drew Schorno <dschorno@gmail.com>
2013-05-13 14:11:14 -0400

    split main.js into bite size chunks

---
+++ javascripts/custom/components/initialization.js
@@ -0,0 +1,27 @@
+$(function(){
+
+        changePage(0);
+
+
+        $(window).on("load resize orientationchange", function() { focalpoint(); });
+        $('.audio').on('click', function(){ toggleSound(); });
+
+
+    $(document).ajaxComplete(function(){
+        
+        stopFilmstrip();
+
+        // if (jQuery.browser.mobile) {
+        if (true) { extractFilmStrip(); };
+
+      filter();
+      fitText();
+      showElementsOnFrame();
+
+      pageview.imagesLoaded(function(){ 
+        focalpoint();
+        frameview.addClass('loaded'); 
+        pageview.addClass('loaded');
+      });
+  },false);
+});
