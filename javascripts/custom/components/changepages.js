4278a24562cc21001a7803ee26cde3898d16a359
Greg Schorno <gschorno@yahoo.com>
2013-05-14 12:18:38 -0400

    page navigation object

--- javascripts/custom/components/changepages.js
+++ javascripts/custom/components/changepages.js
@@ -1,28 +1,37 @@
 
 
-var pageinfo = [
-  { url: "pages/1.html", title: "Introduction", frames: ["pages/1/1.html", "pages/1/2.html", "pages/1/3.html"] },
-  { url: "pages/2.html", title: "Tile", 
-    frames: ["pages/2/1.html", "pages/2/2.html", "pages/2/3.html", "pages/2/4.html", "pages/2/5.html", "pages/2/6.html", "pages/2/7.html"] 
-  },
+var Pages = function () {
+  var pageinfo = [
+    { url: "pages/1.html", title: "Introduction", frames: ["pages/1/1.html", "pages/1/2.html", "pages/1/3.html"] },
+    { url: "pages/2.html", title: "Tile", 
+      frames: ["pages/2/1.html", "pages/2/2.html", "pages/2/3.html", "pages/2/4.html", "pages/2/5.html", "pages/2/6.html", "pages/2/7.html"] 
+    },
 
-];
+    ];
 
-function pageCount() { return pageinfo.length; }
-function getPageTitle(i)  { return pageinfo[i].title; }
+  // todo add ajax loader if needed,
+  // possibly add _pageIndex and _frameIndex to this
 
-function getCurrentFrame() { return pageinfo[_pageIndex].frames[_frameIndex]; }
+  this.pageCount = function () { return pageinfo.length; }
+  this.getPageTitle = function (page)  { return pageinfo[page].title; }
 
-function getPageUrl(page) { return pageinfo[page].url; }
+  this.getPageUrl = function (page) { return pageinfo[page].url; }
 
 
-function getFrames(page) {
-  return pageinfo[page].frames;
-}
+  this.getFrames = function (page) {
+    return pageinfo[page].frames;
+  }
 
-function getFrameCount(page) {
-  return pageinfo[page].frames.length;
+  this.getFrameCount = function (page) {
+    return pageinfo[page].frames.length;
+  };
 };
+
+var _pages = new Pages();
+
+
+function getCurrentFrame() { return _pages.getFrames(_pageIndex)[_frameIndex]; }
+
 
 
     // page navigation code
@@ -59,15 +68,15 @@ function getFrameCount(page) {
             // Fills the navigation with the appropriate links and dropdowns
             //$.each(pages, function(pageNumber){
 
-              for (var i=0; i<pageinfo.length; i++) {
+              for (var i=0; i < _pages.pageCount(); i++) {
                   var navPage = $('<li/>').appendTo(navList);
-                  var navPageLink = $('<a/>').text(getPageTitle(i)).attr('onClick', 'changePage('+ i +')').appendTo(navPage);
+                  var navPageLink = $('<a/>').text(_pages.getPageTitle(i)).attr('onClick', 'changePage('+ i +')').appendTo(navPage);
 
-                  if (getFrameCount(i) > 1) {
+                  if (_pages.getFrameCount(i) > 1) {
                     navPage.addClass('has-dropdown');
                     var navPageList = $('<ul />').addClass('dropdown').appendTo(navPage);
 
-                    var fr = getFrames(i);
+                    var fr = _pages.getFrames(i);
                     $.each(fr, function(frameNumber){
                           var navFrame = $('<li/>').appendTo(navPageList);
                           var navFrameLink = $('<a/>').text('Page ' + (frameNumber + 1)).attr('onClick', 'changePage('+ i +', ' +frameNumber +')').appendTo(navFrame);
@@ -80,7 +89,7 @@ function getFrameCount(page) {
             function changeFrame(value) {
               frameview = $(".frameview").first();
     
-              var frameCount = getFrameCount(_pageIndex), frameEnd = frameCount - 1;
+              var frameCount = _pages.getFrameCount(_pageIndex), frameEnd = frameCount - 1;
               
               if (value==="next") {        _frameIndex = (_frameIndex + 1) % frameCount;
               } else if (value==="prev") { _frameIndex = (_frameIndex + frameCount - 1) % frameCount;
@@ -99,7 +108,7 @@ function getFrameCount(page) {
 
               framecounter.text((_frameIndex+1) + "/" + (frameCount));
 
-              var end = pageCount() - 1;
+              var end = _pages.pageCount() - 1;
               if(_pageIndex === end && _frameIndex === frameEnd){ hideNavNext(); showNavPrev(); }
               else if (_pageIndex === 0 && _frameIndex === 0)   { showNavNext(); hideNavPrev(); }
               else                                            { showNav(); }
@@ -116,19 +125,19 @@ function getFrameCount(page) {
                 }
 
                 pageview.fadeOut('fast', function() { 
-                    pageview.removeClass('loaded').load(getPageUrl(_pageIndex), function() {
+                    pageview.removeClass('loaded').load(_pages.getPageUrl(_pageIndex), function() {
                         frame ? changeFrame(frame) : changeFrame('0');
                         pageview.fadeIn();
                     }); 
                 });
 
-                pagetitle.text(getPageTitle(_pageIndex));
+                pagetitle.text(_pages.getPageTitle(_pageIndex));
               
             }
 
         function next() { 
-            if (_frameIndex < getFrameCount(_pageIndex)) { changeFrame('next'); } 
-            else if(_pageIndex < pageCount()-1){ changePage('next'); }
+            if (_frameIndex < _pages.getFrameCount(_pageIndex)) { changeFrame('next'); } 
+            else if(_pageIndex < _pages.pageCount()-1){ changePage('next'); }
         }; 
 
         function prev() { 
